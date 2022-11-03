7c3c71085de10ee41a21bb97c1ca453097230f97
jon-cron <jonathanacm95@gmail.com>
2022-11-03 14:37:19 -0600

    Attacks coming together

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const heroes = [
   {
-    name: "Slate Slabrock",
+    name: "slate",
     type: "dwarf",
-    damage: 5,
+    lowDamage: 5,
+    highDamage: 10,
     health: 100,
   },
   {
-    name: "Flint Ironstag",
+    name: "flint",
     type: "elf",
-    damage: 10,
+    lowDamage: 5,
+    highDamage: 15,
     health: 50,
   },
 ];
@@ -20,4 +22,33 @@ const boss = {
   damage: 5,
   level: 1,
 };
-console.log("working");
+
+// function weakChance() {
+//   let chances = Math.floor(100 * Math.random());
+//   if (chances > 40) {
+//   }
+//   return false;
+// }
+
+function weakAttack(blank) {
+  boss.health == 100;
+  let player = heroes.find((h) => h.name == blank);
+  let chances = Math.floor(100 * Math.random());
+  if (chances > 40) {
+    boss.health -= player.lowDamage;
+    console.log(boss.health);
+  } else {
+    console.log("missed");
+  }
+}
+function strongAttack(blank) {
+  boss.health == 100;
+  let player = heroes.find((h) => h.name == blank);
+  let chances = Math.floor(100 * Math.random());
+  if (chances > 60) {
+    boss.health -= player.highDamage;
+    console.log(boss.health);
+  } else {
+    console.log("missed");
+  }
+}
