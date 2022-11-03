ce537d17c5fff3deda8f57397c0490947d5eff3e
jon-cron <jonathanacm95@gmail.com>
2022-11-03 14:05:08 -0600

    structure made

---
+++ app.js
@@ -0,0 +1,23 @@
+const heroes = [
+  {
+    name: "Slate Slabrock",
+    type: "dwarf",
+    damage: 5,
+    health: 100,
+  },
+  {
+    name: "Flint Ironstag",
+    type: "elf",
+    damage: 10,
+    health: 50,
+  },
+];
+
+const boss = {
+  name: "boss",
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1,
+};
+console.log("working");
